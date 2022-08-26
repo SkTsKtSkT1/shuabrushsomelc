255be9d958fb36b92c0d4034be0533a8a1dc141a
SktLilivia <M202173116@hust.edu.cn>
2022-08-26 15:09:30 +0800

    22.8.26每日一题

---
+++ simple/leetcode1464.cpp
@@ -0,0 +1,5 @@
+//
+// Created by lzw on 2022/8/26.
+//
+
+#include "leetcode1464.h"
---
+++ simple/leetcode1464.h
@@ -0,0 +1,14 @@
+//
+// Created by lzw on 2022/8/26.
+//
+
+#ifndef ALGORITHM_LEETCODE1464_H
+#define ALGORITHM_LEETCODE1464_H
+
+
+class leetcode1464 {
+
+};
+
+
+#endif //ALGORITHM_LEETCODE1464_H
