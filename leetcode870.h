3696fb582441b9ff99527c8bc9ef3d9aa8bea938
SkTsKtSkT1 <2546786270@qq.com>
2022-10-08 23:57:30 +0800

    22.10.8 每日一题

---
+++ leetcode870.h
@@ -0,0 +1,15 @@
+//
+// Created by skt on 2022/10/8.
+//
+
+#ifndef ALGORITHM_LEETCODE870_H
+#define ALGORITHM_LEETCODE870_H
+#include "vector"
+using std::vector;
+class leetcode870 {
+public:
+    vector<int> advantageCount(vector<int>& nums1, vector<int>& nums2);
+};
+
+
+#endif //ALGORITHM_LEETCODE870_H
