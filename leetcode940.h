4aa22c5c7354274b7b7a42cc775f40859ed5d991
SkTsKtSkT1 <2546786270@qq.com>
2022-10-14 00:40:19 +0800

    22.10.14 每日一题，manuscript给老师了，希望论文一些顺利

---
+++ leetcode940.h
@@ -0,0 +1,20 @@
+//
+// Created by skt on 2022/10/14.
+//
+
+#ifndef ALGORITHM_LEETCODE940_H
+#define ALGORITHM_LEETCODE940_H
+#include "string"
+#include "vector"
+using std::string;
+using std::vector;
+
+class leetcode940 {
+private:
+    long _mod=1e9+7;
+public:
+    int distinctSubseqII(string s);
+};
+
+
+#endif //ALGORITHM_LEETCODE940_H
--- main.cpp
+++ main.cpp
@@ -1,12 +1,11 @@
 #include <iostream>
-#include "leetcode769.h"
+#include "leetcode940.h"
 
 
 int main() {
-    leetcode769 lc769;
-    vector<int> arr={1,2,0,3};
-    //vector<int> arr={1,0,2,3,4};
-    std::cout<<lc769.maxChunksToSorted(arr)<<std::endl;
+    leetcode940 lc940;
+    string s="zz";
+    std::cout<<lc940.distinctSubseqII(s)<<std::endl;
     return 0;
 }
 
