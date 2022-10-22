4aa5c01c5fccb0375e37b2d88e3ff88652fbbc29
SktLilivia <M202173116@hust.edu.cn>
2022-10-22 23:22:04 +0800

    22.10.22 每日一题 今天去玩了 摸

---
+++ leetcode1235.h
@@ -0,0 +1,16 @@
+//
+// Created by lzw on 2022/10/22.
+//
+
+#ifndef ALGORITHM_LEETCODE1235_H
+#define ALGORITHM_LEETCODE1235_H
+#include "vector"
+#include "algorithm"
+using std::vector;
+class leetcode1235 {
+public:
+        int jobScheduling(vector<int> &startTime, vector<int> &endTime, vector<int> &profit);
+};
+
+
+#endif //ALGORITHM_LEETCODE1235_H
