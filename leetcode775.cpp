4c7ee14b44eec6d9ae970c030b7b8c498cfca357
SktLilivia <M202173116@hust.edu.cn>
2022-11-16 14:12:46 +0800

    22.11.16 每日一题

---
+++ leetcode775.cpp
@@ -0,0 +1,22 @@
+//
+// Created by lzw on 2022/11/16.
+//
+
+#include "leetcode775.h"
+#include "cmath"
+bool leetcode775::isIdealPermutation(vector<int> &nums) {
+    //要求一个局部倒置就是一个全局倒置，也就是倒置的长度应该为2； 思路对了但是超时了。。
+//    for(int i=0;i<nums.size()-1;i++){
+//        for(int j=i+2;j<nums.size();j++){
+//            if(nums[j]<nums[i]) return false;
+//        }
+//    }
+//    return true;
+    //如果数列进行排序后，数字是和下标一一对应，如果这个数和下标差距大于1，则return false;
+    for(int i=0;i<nums.size();i++){
+        if(nums[i]!=i && abs(nums[i]-i)>1){
+            return false;
+        }
+    }
+    return true;
+}
---
+++ leetcode775.h
@@ -0,0 +1,16 @@
+//
+// Created by lzw on 2022/11/16.
+//
+
+#ifndef ALGORITHM_LEETCODE775_H
+#define ALGORITHM_LEETCODE775_H
+#include "vector"
+using std::vector;
+
+class leetcode775 {
+public:
+    bool isIdealPermutation(vector<int>& nums);
+};
+
+
+#endif //ALGORITHM_LEETCODE775_H
