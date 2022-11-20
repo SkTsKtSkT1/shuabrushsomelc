6ff314dd6ba48f6d1b4f3f33313bf1cdd3d88485
SktLilivia <M202173116@hust.edu.cn>
2022-11-20 15:45:45 +0800

    22.11.20 每日一题

---
+++ leetcode799.h
@@ -0,0 +1,14 @@
+//
+// Created by lzw on 2022/11/20.
+//
+
+#ifndef ALGORITHM_LEETCODE799_H
+#define ALGORITHM_LEETCODE799_H
+
+
+class leetcode799 {
+    double champagneTower(int poured, int query_row, int query_glass);
+};
+
+
+#endif //ALGORITHM_LEETCODE799_H
