4cec039395b6fc0b48411bc03507b410d270b872
SkTsKtSkT1 <2546786270@qq.com>
2023-05-22 17:12:44 +0800

    23.5.22,daily.

---
+++ middle/leetcode1080.cpp
@@ -0,0 +1,15 @@
+//
+// Created by 25467 on 2023/5/22.
+//
+
+#include "leetcode1080.h"
+
+TreeNode *leetcode1080::sufficientSubset(TreeNode *root, int limit) {
+    if(root == nullptr) return nullptr;
+    if(root->left == nullptr && root->right == nullptr) return root->val < limit?nullptr:root;
+
+    root->left = sufficientSubset(root->left, limit - root->val);
+    root->right = sufficientSubset(root->right, limit - root->val);
+
+    return root->left == nullptr && root->right == nullptr ? nullptr : root;
+}
---
+++ middle/leetcode1080.h
@@ -0,0 +1,15 @@
+//
+// Created by 25467 on 2023/5/22.
+//
+
+#ifndef ALGORITHM_LEETCODE1080_H
+#define ALGORITHM_LEETCODE1080_H
+#include "../TreeNode.h"
+
+class leetcode1080 {
+public:
+    TreeNode* sufficientSubset(TreeNode* root, int limit);
+};
+
+
+#endif //ALGORITHM_LEETCODE1080_H
