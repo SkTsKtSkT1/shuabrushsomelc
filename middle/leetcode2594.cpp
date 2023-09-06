6e6bb28084defda424e3ab92f6cec8f5edd0473d
SkTsKtSkT1 <2546786270@qq.com>
2023-09-07 00:32:55 +0800

    23.9.7,daily

---
+++ middle/leetcode2594.cpp
@@ -0,0 +1,29 @@
+//
+// Created by skt on 2023/9/7.
+//
+
+#include "leetcode2594.h"
+#include <numeric>
+long long leetcode2594::repairCars(std::vector<int> &ranks, int cars) {
+    long long curTime = *std::min_element(ranks.begin(), ranks.end()) * (cars * cars);
+    long long left = 0, right = curTime;
+    //(left , right)
+    while(right > left + 1){
+        long long mid = left + (right - left) / 2;
+        if(carNum(ranks, mid) >= cars){ //大于等于都要收缩，因为要找到最小的边界
+            right = mid;
+        }else{
+            left = mid;
+        }
+    }
+    return right;
+}
+
+long long leetcode2594::carNum(std::vector<int> &ranks, long long time) {
+    // t = r * n ^2 -> n = sqrt(t /r )
+    long long total = 0;
+    for(int rank : ranks){
+        total += std::sqrt(time / rank);
+    }
+    return total;
+}
---
+++ middle/leetcode2594.h
@@ -0,0 +1,17 @@
+//
+// Created by skt on 2023/9/7.
+//
+
+#ifndef ALGORITHM_LEETCODE2594_H
+#define ALGORITHM_LEETCODE2594_H
+
+#include <vector>
+class leetcode2594 {
+public:
+    long long repairCars(std::vector<int>& ranks, int cars);
+
+    long long carNum(std::vector<int> &ranks, long long time);
+};
+
+
+#endif //ALGORITHM_LEETCODE2594_H
