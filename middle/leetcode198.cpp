eac4d591e0e7af7da5052a0b706ddfd55f2f05b7
SkTsKtSkT1 <2546786270@qq.com>
2023-09-17 20:17:21 +0800

    23.9.17,daily.

---
+++ middle/leetcode198.cpp
@@ -0,0 +1,19 @@
+//
+// Created by skt on 2023/9/16.
+//
+
+#include "leetcode198.h"
+#include <cstring>
+//https://leetcode.cn/problems/house-robber/solutions/138131/dong-tai-gui-hua-jie-ti-si-bu-zou-xiang-jie-cjavap/?envType=daily-question&envId=2023-09-16
+int leetcode198::rob(std::vector<int> &nums) {
+    int n = nums.size();
+    //dp[i] 表示前i间的最大收益[1, n)
+    int dp[n + 1];
+    memset(dp, 0, sizeof(dp));
+    dp[1] = nums[0];
+    for(int i = 2; i < n + 1; ++i){
+        //偷前i - 1个 or 前i - 2个和 当前房子
+        dp[i] = std::max(dp[i - 2] + nums[i - 1], dp[i - 1]);
+    }
+    return dp[n];
+}
---
+++ middle/leetcode198.h
@@ -0,0 +1,16 @@
+//
+// Created by skt on 2023/9/16.
+//
+
+#ifndef ALGORITHM_LEETCODE198_H
+#define ALGORITHM_LEETCODE198_H
+
+#include <vector>
+
+class leetcode198 {
+public:
+    int rob(std::vector<int>& nums);
+};
+
+
+#endif //ALGORITHM_LEETCODE198_H
